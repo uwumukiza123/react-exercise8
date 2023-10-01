483ed8a575171f8d19f2ad8e62937044dd22b64d
uwumukiza123 <uwumukizalaetitia@gmail.com>
2023-10-01 11:53:30 +0200

    add react exercise8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import programs from "./example.json";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main">
+        {programs.map((program) => (
+          <div className="program" key={program.id}>
+            <h2 className="heading">{program.setup}</h2>
+            <p className="para">{program.punchline}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
